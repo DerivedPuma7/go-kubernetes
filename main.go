2e827a5c8403910457108ea9eb37d02035773258
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2025-01-30 12:23:52 -0300

    feat: reading data from a mounted volume

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
 	http.ListenAndServe(":3000", nil)
 }
 
@@ -16,4 +18,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 	
 	fmt.Fprintf(w, "Hello! I'm %s, I'm %s", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("/app/myFamily/family.txt")
+	if err != nil {
+		log.Fatal("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "My family: %s", string(data))
 }
