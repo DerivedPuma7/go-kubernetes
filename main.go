489955696c82cea266159dfb123acc7d3ba9bc77
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2025-01-30 14:51:07 -0300

    feat: using secrets from k8s

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	http.HandleFunc("/", Hello)
 	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 	http.ListenAndServe(":3000", nil)
 }
 
@@ -26,4 +27,11 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("Error reading file: ", err)
 	}
 	fmt.Fprintf(w, "My family: %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	pass := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s\nPassword: %s", user, pass)
 }
